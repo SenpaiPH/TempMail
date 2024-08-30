c6b6f9527a695ffd4126b47819c7cce16efaf369
SenpaiPH <166698154+SenpaiPH@users.noreply.github.com>
2024-08-30 13:37:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const getRandomEmail = require('./getRandomEmail');
-
+const getAllEmailMsg = require('./getAllEmailMsg');
 const app = express();
 
 app.get('/random-email', async (req, res) => {
@@ -12,7 +12,16 @@ app.get('/random-email', async (req, res) => {
     res.status(500).json({ message: 'Error generating random email' });
   }
 });
-
+app.get('/all-emails', async (req, res) => {
+  try {
+    const { login, domain } = req.query; // or req.body, depending on how you want to pass the parameters
+    const emailData = await getAllEmailMsg(login, domain);
+    res.json(emailData);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ message: 'Error fetching all emails' });
+  }
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
