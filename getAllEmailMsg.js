93051e9b3c09b8ecbe35f59dc052add01ee28603
SenpaiPH <166698154+SenpaiPH@users.noreply.github.com>
2024-08-30 13:38:33 +0800

    Create getAllEmailMsg.js

---
+++ getAllEmailMsg.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+async function fetchEmails(login, domain) {
+  const apiUrl = https://www.1secmail.com/api/v1/?action=getMessages&login=${login}&domain=${domain};
+
+  try {
+    const response = await axios.get(apiUrl);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching emails:', error);
+    throw error;
+  }
+}
+
+module.exports = fetchEmails;
